33c2ed09921ffc97a7ea423ffbe1d3b3a5b7abb3
lei335 <1019764004@qq.com>
2022-03-02 11:16:01 +0800

    third commit

--- db/postgresql.go
+++ db/postgresql.go
@@ -5,8 +5,10 @@ import (
 	"fmt"
 )
 
+var pgdb *sql.DB
+
 type UserInfo struct {
-	UID        int64
+	UID        int
 	UserName   string
 	Department string
 	Created    string
@@ -14,14 +16,15 @@ type UserInfo struct {
 
 // ConnectPQ connect postgresql
 func ConnectPQ() (*sql.DB, error) {
+	var err error
 
 	dbDsn := "user=postgres password=123456 dbname=recordings sslmode=disable"
-	db, err := sql.Open("postgres", dbDsn)
+	pgdb, err = sql.Open("postgres", dbDsn)
 	if err != nil {
 		return nil, err
 	}
 
-	pingErr := db.Ping()
+	pingErr := pgdb.Ping()
 	if pingErr != nil {
 		return nil, pingErr
 	}
@@ -30,22 +33,68 @@ func ConnectPQ() (*sql.DB, error) {
 	return db, nil
 }
 
-func InsertData(user *UserInfo) error {
-	db, err := ConnectPQ()
+func InsertData(user *UserInfo) (int, error) {
+
+	stmt, err := pgdb.Prepare("INSERT INTO userinfo(username,department,created) VALUES($1,$2,$3) RETURNING uid")
+	if err != nil {
+		return 0, err
+	}
+
+	_, err = stmt.Exec(user.UserName, user.Department, user.Created)
+	if err != nil {
+		return 0, err
+	}
+
+	_, err = stmt.Exec("alex", "产品部门", "2019-07-01")
+	if err != nil {
+		return 0, err
+	}
+
+	var lastInsertID int
+	// 会执行插入操作并返回lastInsertID
+	err = pgdb.QueryRow("INSERT INTO userinfo(username,department,created) VALUES($1,$2,$3) returning uid;", "alex", "产品部门", "2019-07-01").Scan(&lastInsertID)
+	if err != nil {
+		return 0, err
+	}
+	fmt.Println("最后插入id = ", lastInsertID)
+	return lastInsertID, nil
+}
+
+func UpdateData(user *UserInfo) error {
+
+	stmt, err := pgdb.Prepare("update userinfo set username=$1 where uid=$2")
 	if err != nil {
 		return err
 	}
 
-	stmt, err := db.Prepare("INSERT INTO userinfo(username,department,created) VALUES($1,$2,$3) RETURNING uid")
+	res, err := stmt.Exec(user.UserName, user.UID)
 	if err != nil {
 		return err
 	}
 
-	res, err := stmt.Exec("alex", "产品部门", "2019-07-01")
+	affect, err := res.RowsAffected()
 	if err != nil {
 		return err
 	}
 
-	var lastInsertId int
-	err = db.QueryRow("", )
+	fmt.Println("update affected nums: ", affect)
+	return nil
+}
+
+func QueryData() error {
+
+	rows, err := pgdb.Query("SELECT * FROM userinfo")
+	if err != nil {
+		return err
+	}
+
+	uinfo := &UserInfo{}
+	for rows.Next() {
+		err = rows.Scan(&uinfo.UID, &uinfo.UserName, &uinfo.Department, &uinfo.Created)
+		if err != nil {
+			return err
+		}
+		fmt.Printf("uid: %v, username: %v, department: %v, created: %v\n", uinfo.UID, uinfo.UserName, uinfo.Department, uinfo.Created)
+	}
+	return nil
 }
--- main.go
+++ main.go
@@ -8,9 +8,41 @@ import (
 )
 
 func main() {
-	db, err := godb.ConnectPQ()
+	_, err := godb.ConnectPQ()
 	if err != nil {
 		log.Fatal(err)
 	}
-	db.Close()
+
+	// 插入数据
+	uinfo := &godb.UserInfo{
+		UserName:   "Lily",
+		Department: "UI部门",
+		Created:    "2021-10-08",
+	}
+	lastInsertID, err := godb.InsertData(uinfo)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// 查询数据
+	err = godb.QueryData()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// 更新数据
+	uinfo = &godb.UserInfo{
+		UserName: "Bob",
+		UID:      lastInsertID,
+	}
+	err = godb.UpdateData(uinfo)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// 查询数据
+	err = godb.QueryData()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
